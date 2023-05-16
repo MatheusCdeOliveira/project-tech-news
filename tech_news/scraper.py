20433ae4e0114ef5852464311a8bde0bc9d0c971
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-05-16 17:35:09 -0300

    some lines refactored

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -32,11 +32,10 @@ def scrape_updates(html_content):
 def scrape_next_page_link(html_content):
     selector = Selector(text=html_content)
     content = selector.css(".next.page-numbers::attr(href)").get()
-    print(type(content))
-    if content == "":
-        return None
-    else:
+    if content:
         return content
+    else:
+        return None
 
 
 # Requisito 4
