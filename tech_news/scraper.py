ec41fb379fd215fe27004d00b46de749c78982ac
Matheus Campos de Oliveira <matheuswe2010@hotmail.com>
2023-05-16 17:32:51 -0300

    req 03 done

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -10,11 +10,12 @@ def fetch(url):
     try:
         data = requests.get(url, headers=user_agent, timeout=3)
         time.sleep(1)
-        if data.status_code != 200:
-            return None
-        return data.text
     except requests.ReadTimeout:
         return None
+
+    if data.status_code != 200:
+        return None
+    return data.text
 
 
 # Requisito 2
@@ -29,7 +30,13 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    content = selector.css(".next.page-numbers::attr(href)").get()
+    print(type(content))
+    if content == "":
+        return None
+    else:
+        return content
 
 
 # Requisito 4
